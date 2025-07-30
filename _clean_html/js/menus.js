022dcf93bc0fee4ea8041a7eab6f8e5ef133023a
ShakirKhun <khunakhunovshakir@gmail.com>
2025-07-30 15:18:57 +0500

    Добавил все меню, все анимации, главная страница готова

--- _clean_html/js/menus.js
+++ _clean_html/js/menus.js
@@ -1,5 +1,3 @@
-console.log("hh");
-
 document.addEventListener('DOMContentLoaded', () => {
     const toggleBtn = document.getElementById('menu-toggle');
     const menu = document.getElementById('hidden-nav');
@@ -30,36 +28,36 @@ document.addEventListener('DOMContentLoaded', () => {
         let timeout;
 
         function showMenu() {
-        clearTimeout(timeout);
-        menu.style.display = 'block';
+            clearTimeout(timeout);
+            menu.style.display = 'block';
         }
 
         function hideMenu() {
-        timeout = setTimeout(() => {
-            if (!isHovering) {
-            menu.style.display = 'none';
-            }
-        }, 200);
+            timeout = setTimeout(() => {
+                if (!isHovering) {
+                menu.style.display = 'none';
+                }
+            }, 200);
         }
 
         button.addEventListener('mouseenter', () => {
-        isHovering = true;
-        showMenu();
+            isHovering = true;
+            showMenu();
         });
 
         button.addEventListener('mouseleave', () => {
-        isHovering = false;
-        hideMenu();
+            isHovering = false;
+            hideMenu();
         });
 
         menu.addEventListener('mouseenter', () => {
-        isHovering = true;
-        showMenu();
+            isHovering = true;
+            showMenu();
         });
 
         menu.addEventListener('mouseleave', () => {
-        isHovering = false;
-        hideMenu();
+            isHovering = false;
+            hideMenu();
         });
     }
 
@@ -69,5 +67,78 @@ document.addEventListener('DOMContentLoaded', () => {
     setupHover('policy-button', 'policy-menu');
     setupHover('review-button', 'review-menu');
     setupHover('search-button', 'search-menu');
+
+    function setupButton(buttonId, menuId, navIcon) {
+        const button = document.getElementById(buttonId);
+        const menu = document.getElementById(menuId);
+        const icon = document.getElementById(navIcon);
+        
+        button.addEventListener('click', () => {
+            const currentDisplay = getComputedStyle(menu).display;
+
+            if (currentDisplay === 'none') {
+                menu.style.display = 'block';
+                icon.style.transform = 'rotate(0deg)';
+            } else {
+                menu.style.display = 'none';
+                icon.style.transform = 'rotate(-90deg)';
+            }
+
+        });
+    }
+
+    setupButton('navJournalButton', 'navJournalMenu', 'navJournalIcon');
+    setupButton('navAuthorButton', 'navAuthorMenu', 'navAuthorIcon');
+    setupButton('navPolicyButton', 'navPolicyMenu', 'navPolicyIcon');
+    setupButton('navReviewButton', 'navReviewMenu', 'navReviewIcon');
+
+
+    function setupHoverLang() {
+        const button = document.getElementById('langChangeButton');
+        const menu = document.getElementById('hidden-lang');
+
+        let isHovering = false;
+        let timeout;
+
+        function showMenu() {
+            clearTimeout(timeout);
+            menu.style.display = 'block';
+            button.style.borderBottom = 'none';
+            button.style.borderRadius = '5px 5px 0 0';
+        }
+
+        function hideMenu() {
+            timeout = setTimeout(() => {
+                if (!isHovering) {
+                menu.style.display = 'none';
+                button.style.borderBottom = '1.5px solid #D9D9D9';
+                button.style.borderRadius = '5px';
+                }
+            }, 200);
+        }
+
+        button.addEventListener('mouseenter', () => {
+            isHovering = true;
+            showMenu();
+        });
+
+        button.addEventListener('mouseleave', () => {
+            isHovering = false;
+            hideMenu();
+        });
+
+        menu.addEventListener('mouseenter', () => {
+            isHovering = true;
+            showMenu();
+        });
+
+        menu.addEventListener('mouseleave', () => {
+            isHovering = false;
+            hideMenu();
+        });
+    }
+
+    setupHoverLang();
+
 })
 
